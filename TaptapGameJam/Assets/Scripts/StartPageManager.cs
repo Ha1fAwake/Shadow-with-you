1e984473473edee8c1ef6822c9a27924f9090135
QuanMao <2541673770@qq.com>
2019-12-14 17:23:48 +0800

    StartPage 12.14 17:23

---
+++ TaptapGameJam/Assets/Scripts/StartPageManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartPageManager : MonoBehaviour
+{
+    public void StartBtn()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitBtn()
+    {
+        Application.Quit();
+    }
+
+    public void HelpBtn()
+    {
+        Application.OpenURL("https://shimo.im/docs/6YyG6XCWj6KpWpRG");
+    }
+}

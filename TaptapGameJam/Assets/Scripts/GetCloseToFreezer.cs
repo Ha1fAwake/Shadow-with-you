656d8b4b3895c658729bfc2ca8239c72b8516618
QuanMao <2541673770@qq.com>
2019-12-15 00:23:13 +0800

    0:23

---
+++ TaptapGameJam/Assets/Scripts/GetCloseToFreezer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class GetCloseToFreezer : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "Player")
+        {
+            Flowchart flowchart = GameObject.Find("Flowchart").GetComponent<Flowchart>();
+            flowchart.ExecuteBlock("GetCloseToFreezer");
+        }
+    }
+}

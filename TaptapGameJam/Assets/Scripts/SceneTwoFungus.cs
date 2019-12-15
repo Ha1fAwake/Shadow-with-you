7dffb26a7e873eb53d2f50f46e6bde49fc18f7a1
QuanMao <2541673770@qq.com>
2019-12-15 08:40:10 +0800

    8:40

---
+++ TaptapGameJam/Assets/Scripts/SceneTwoFungus.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class SceneTwoFungus : MonoBehaviour
+{
+    public Transform cameraMain;
+    public void MoveCameraToTop()
+    {
+        cameraMain.DOMoveY(0, 0.3f);
+    }
+
+    public void BanController()
+    {
+        EventCenter.Broadcast<bool>(MyEventType.changeController, false);
+    }
+
+    public void EnableController()
+    {
+        EventCenter.Broadcast<bool>(MyEventType.changeController, true);
+    }
+}
